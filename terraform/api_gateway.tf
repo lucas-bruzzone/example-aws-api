50b5b13f5253203d4c579e5258cc7bb1de67e365
lucas-bruzzone <lucas.rbruzzone@gmail.com>
2025-07-14 14:45:01 -0300

    feat: add API Gateway resources and Lambda integration

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -7,3 +7,67 @@ resource "aws_api_gateway_rest_api" "main" {
     types = ["REGIONAL"]
   }
 }
+
+# Recurso /validate
+resource "aws_api_gateway_resource" "validate" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "validate"
+}
+
+# Método POST /validate
+resource "aws_api_gateway_method" "validate_post" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.validate.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+# Integração com Lambda
+resource "aws_api_gateway_integration" "validate_lambda" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.validate.id
+  http_method = aws_api_gateway_method.validate_post.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = data.terraform_remote_state.lambda.outputs.lambda_invoke_arn
+}
+
+# Permissão para API Gateway invocar Lambda
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = data.terraform_remote_state.lambda.outputs.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+}
+
+# Deployment
+resource "aws_api_gateway_deployment" "main" {
+  depends_on = [
+    aws_api_gateway_method.validate_post,
+    aws_api_gateway_integration.validate_lambda,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.main.id
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.validate.id,
+      aws_api_gateway_method.validate_post.id,
+      aws_api_gateway_integration.validate_lambda.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Stage
+resource "aws_api_gateway_stage" "main" {
+  deployment_id = aws_api_gateway_deployment.main.id
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  stage_name    = var.environment
+}
