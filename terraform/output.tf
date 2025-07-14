8d62bcad5393ffa6115db4880732abef0fd78ff6
lucas-bruzzone <lucas.rbruzzone@gmail.com>
2025-07-14 11:16:38 -0300

    feat: add API Gateway resources and outputs

---
+++ terraform/output.tf
@@ -0,0 +1,24 @@
+output "api_gateway_id" {
+  description = "ID do API Gateway"
+  value       = aws_api_gateway_rest_api.main.id
+}
+
+output "api_gateway_root_resource_id" {
+  description = "ID do recurso raiz do API Gateway"
+  value       = aws_api_gateway_rest_api.main.root_resource_id
+}
+
+output "api_gateway_execution_arn" {
+  description = "ARN de execução do API Gateway"
+  value       = aws_api_gateway_rest_api.main.execution_arn
+}
+
+output "api_gateway_url" {
+  description = "URL do API Gateway"
+  value       = "https://${aws_api_gateway_rest_api.main.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment}"
+}
+
+output "api_gateway_stage_name" {
+  description = "Nome do stage"
+  value       = aws_api_gateway_stage.main.stage_name
+}
